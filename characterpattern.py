21af8810ed69b0ee69c8d9ab50860a3d11f20f25
rahul kahar 88 <rahulkahar88588@gmail.com>
2025-11-16 00:11:01 +0530

    update

---
+++ characterpattern.py
@@ -0,0 +1,29 @@
+# ======================================= letter pattern ==============================================
+
+# x = 'A'
+# print(ord(x))
+# print(ord(x)+1)
+# print(chr(ord(x)+1))
+
+
+
+
+# n = int(input("enter any number : "))
+# ch='A'
+# for i in range (n):
+#     print(ch)
+#     ch = chr(ord(ch)+1)
+
+
+
+
+
+
+# n = int(input("enter munber"))
+# for i in range (n):
+#     ch = 'A'
+#     for j in range (n):
+#         print(ch,end=' ')
+#         ch = chr(ord(ch)+1)
+#     print()
+

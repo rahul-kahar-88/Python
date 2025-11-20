982bf947b77c712c1002aa68c3d3b79be31ce17d
rahul kahar 88 <rahulkahar88588@gmail.com>
2025-11-20 20:42:38 +0530

    update

---
+++ function.py
@@ -0,0 +1,103 @@
+
+
+#   1 = In-build function ---
+#   2 = user define function ---
+#
+#
+# 
+
+# =================  syntex of user defined ================
+#
+#
+#                   required variable in function body
+#                       ^
+#                       |
+#   def fun-name(parameter):
+#        |
+#        |  fun-body
+#        |
+#        |   return
+#   fun-name(arguments)  --->   value again parameter
+
+
+
+
+
+
+
+
+# def hey():
+#     print("hey rahul")
+# hey()
+
+
+
+
+# def xyz():
+#     pass
+# xyz()
+
+
+
+
+
+# def add (x,y):
+#     print(x+y)
+#     return None
+# p=int(input("enter any number : "))
+# q=int(input("enter any number : "))
+# res=add(p,q)
+# print(res)
+# print(res)
+# print(res)
+# print(res)
+
+
+
+
+
+# def add (x,y):
+#     z=x+y
+#     return z
+# p=int(input("enter any number : "))
+# q=int(input("enter any number : "))
+# res=add(p,q)
+# print(res)
+# print(res)
+# print(res)
+# print(res)
+
+
+
+
+# print(print("hey"))
+
+
+
+
+# def add (x,y):
+#     print(x+y)
+# p=10
+# q=20
+# print(add(p,q))
+
+
+
+
+
+# def add (x,y):
+#     print(x+y)
+#     return (x+y)
+# p=10
+# q=20
+# print(add(p,q))
+
+
+
+
+
+# user define function  ==================== 
+#  with argument   -----  1, with return     2, without return
+#  without argument  ----  1, with return     2, without return 
+
+# function me return use kerna hi padega Python me 
